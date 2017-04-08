1cf3c69b127aac0fa465a7486b9e090805b11829
c0d0er <laeuferk@gmail.com>
2017-04-07 18:03:30 -0700

    Update fscript.js

--- fscript.js
+++ fscript.js
@@ -5,15 +5,6 @@ class ForceGraph extends React.Component{
       let links=data.links;
       let w=window.innerWidth;
       let h=window.innerHeight;
-      //console.log(nodes)
-      //console.log(links)
-
-      let linksArr=[];
-      links.forEach((e,i)=>{
-        console.log(e)
-        linksArr.push([e.source, e.target])
-      })
-console.log(linksArr)
 
       let div = d3.select('.forcegraph').append('div');
 
@@ -51,17 +42,72 @@ console.log(linksArr)
                             .on('end', dragended)
                             );
 
+
+ let linkedByIndex = {};
+    links.forEach(function(d) {
+
+        linkedByIndex[d.source.index + "," + d.target.index] = 1;
+    });
+  console.log(linkedByIndex)
+    
+    function isConnected(a, b) {
+        return linkedByIndex[a.index + "," + b.index] || linkedByIndex[b.index + "," + a.index];
+    }
+/*    node.on("mouseover", function(d){
+        node.classed("node-connected", function(o) {
+console.log(o)
+            return isConnected(d, o);
+        });
+
+        link.classed("link-active", function(o) {
+            return o.source === d || o.target === d;
+        });
+
+        d3.select(this).classed('node-active', true);
+    }).on("mouseout", function(d){                
+        node.attr('class', 'node')
+        link.classed("link-active", false);
+    });
+
+    */
+
           node.on('mouseover', function(d){
-            console.log(d)
-                      div.html('<div class="tooltip1"><span class="country">'+d.country+d.index+'</span></div>')
-            .style('left', (d3.event.clientX) + 'px')
-            .style('top', (d3.event.clientY - 32) + 'px')
+                      let tooltipCountries=''
+                      let connectedCountries=[];
+            node.style('transform', function(o){
+              if(isConnected (d,o)){
+                connectedCountries.push(o.country);
+                tooltipCountries=d.country+', is connected to '+connectedCountries.join(', ')+'.';
+                return 'scale(1.2)';}
+                else{return 'scale(0.7)';}
+            })
+
+            node.classed('node-connected', function(o){
+              return isConnected(d,0);
+            });
+            link.classed("link-active", function(o) {
+            return o.source === d || o.target === d;
+        });
+
+            d3.select(this).style('transform', 'scale(2)');
+
+            div.html('<div class="tooltip1"><span class="country">'+tooltipCountries+'</span></div>')
+            .style('left', (d3.event.clientX-24) + 'px')
+            .style('top', (d3.event.clientY - 52) + 'px')
             .style('position', 'absolute');
+
       })
 
               .on('mouseout', function(){
                 d3.select('.tooltip1')
             .classed('hidden', 'true');
+
+            node.style('transform', 'scale(1)')
+
+            node.classed('node-connected', false);
+            link.classed('link-active', false);
+            d3.select(this).style('transform', 'scale(1)')
+
           });
 
           function ticked() {
@@ -98,7 +144,7 @@ console.log(linksArr)
     }
 
     svg.append('text')
-        .attr("transform", "translate(" + (w / 2) + " ," + 60 + ")")
+        .attr("transform", "translate(" + (w / 2) + " ," + 45 + ")")
         .style("text-anchor", "middle")
         .attr('fill', 'white')
         .style('font-size', '1.9em')
